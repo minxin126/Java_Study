aff6d5291989187373bd8e86e9ad40278caa7cb1
汪仁开 <wangrenkai@meituan.com>
2017-09-13 10:22:05 +0800

    format files

--- small.java
+++ small.java
@@ -1,34 +1,34 @@
- public class small{
+public class small {
 
-public static void main(String args[]){
+	public static void main(String args[]) {
 
-int count=0;
+		int count = 0;
 
-int c;
+		int c;
 
-for(int i=1;i<5;i++)
+		for (int i = 1; i < 5; i++)
 
-for(int j=1;j<5;j++)
+			for (int j = 1; j < 5; j++)
 
-for(int k=1;k<5;k++){
+				for (int k = 1; k < 5; k++) {
 
-if(i!=j&&i!=k&&j!=k){
+					if (i != j && i != k && j != k) {
 
-count++;
+						count++;
 
-c=100*i+10*j+k;
+						c = 100 * i + 10 * j + k;
 
-System.out.print(c+" ");
+						System.out.print(c + " ");
 
-if(count%10==0){System.out.println();}
+						if (count % 10 == 0) {System.out.println();}
 
-}
+					}
 
-}
+				}
 
-System.out.println("\n符合要求的数个数为："+count);
+		System.out.println("\n符合要求的数个数为：" + count);
 
-}
+	}
 
 }
 
