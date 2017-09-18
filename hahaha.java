1fb20a94f4581fbd8b279813bab8a22e76821dab
minxin126 <wangminxin6@126.com>
2017-09-18 22:55:02 +0800

    youshichaode

---
+++ hahaha.java
@@ -0,0 +1,20 @@
+class hahaha
+{
+    public static void main(String args[])
+    {
+      float x=12.56f,y;
+      if(x<=0)
+      {
+        y=x+1;
+      }
+      else if(x>0&&x<=16)
+      {
+        y=2*x+1;
+      }
+      else 
+      {
+         y=3*x+3;
+      }
+      System.out.println(y);
+    }
+}
